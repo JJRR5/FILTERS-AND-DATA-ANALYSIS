83524166cd3f4d335d63d3dcb51fc7309ab7a291
Crismedinar <78812546+Crismedinar@users.noreply.github.com>
2021-06-12 12:10:13 -0500

    Interfaz comentada

--- PROYECTO.py
+++ PROYECTO.py
@@ -11,6 +11,8 @@ ventana.title("Proyecto: Generación de filtros digitales")
 frame=Frame(ventana)
 frame.pack()
 
+#Segunda ventana
+
 def select():
 	filtro=menu1.current()
 	tipo=menu2.current()
@@ -27,6 +29,10 @@ def select():
 		fc=DoubleVar()
 		fc1=DoubleVar()
 		fc2=DoubleVar()
+		rp=DoubleVar()
+		rs=DoubleVar()
+
+		#Orden y frecuencia de muestreo (Todos la necesitan)
 		
 		labelN=Label(frame2,text="Orden del filtro:")
 		labelN.grid(row=1,column=1)
@@ -42,6 +48,8 @@ def select():
 		entryfs.grid(row=2,column=2,padx=10,pady=10)
 		entryfs.config(justify="right")
 
+		#Frecuencia de corte para pasa bajas y pasa altas
+
 		if tipo==0 or tipo==1:
 			labelfc=Label(frame2,text="Frecuencia de corte:")
 			labelfc.grid(row=3,column=1)
@@ -49,6 +57,8 @@ def select():
 			entryfc=Entry(frame2,textvariable=fc)
 			entryfc.grid(row=3,column=2,padx=10,pady=10)
 			entryfc.config(justify="right")
+
+		#Frecuencias de corte para pasa banda y rechazo de banda
 		
 		elif tipo==2 or tipo==3:
 			labelfc1=Label(frame2,text="Frecuencia de corte inferior:")
@@ -65,34 +75,40 @@ def select():
 			entryfc2.grid(row=4,column=2,padx=10,pady=10)
 			entryfc2.config(justify="right")
 		
+		#Filtro cheby1
+
 		if filtro==1:
 			labelrp=Label(frame2,text="Rizo en la región de pasa banda:")
 			labelrp.grid(row=5,column=1)
 
-			entryrp=Entry(frame2,textvariable=fc1)
+			entryrp=Entry(frame2,textvariable=rp)
 			entryrp.grid(row=5,column=2,padx=10,pady=10)
 			entryrp.config(justify="right")
 
+		#Filtro cheby2
+
 		elif filtro==2:
-			labelrs=Label(frame2,text="Rizo en la región de rechaso:")
+			labelrs=Label(frame2,text="Rizo en la región de rechazo:")
 			labelrs.grid(row=6,column=1)
 
-			entryrs=Entry(frame2,textvariable=fc1)
+			entryrs=Entry(frame2,textvariable=rs)
 			entryrs.grid(row=6,column=2,padx=10,pady=10)
 			entryrs.config(justify="right")
+
+		#Filtro ellip
 
 		elif filtro==3:
 			labelrp=Label(frame2,text="Rizo en la región de pasa banda:")
 			labelrp.grid(row=5,column=1)
 
-			entryrp=Entry(frame2,textvariable=fc1)
+			entryrp=Entry(frame2,textvariable=rp)
 			entryrp.grid(row=5,column=2,padx=10,pady=10)
 			entryrp.config(justify="right")
 
-			labelrs=Label(frame2,text="Rizo en la región de rechaso:")
+			labelrs=Label(frame2,text="Rizo en la región de rechazo:")
 			labelrs.grid(row=6,column=1)
 
-			entryrs=Entry(frame2,textvariable=fc1)
+			entryrs=Entry(frame2,textvariable=rs)
 			entryrs.grid(row=6,column=2,padx=10,pady=10)
 			entryrs.config(justify="right")
 			
